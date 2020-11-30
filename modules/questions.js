0da42eaef8416c4859b9ecb777b3381d39c689a9
Daniel Rodrigues <rodrig75@coventry.ac.uk>
2020-11-30 20:29:46 +0000

    reads sql info into js adds towards the feature of displaying question data on handlebar a new file added that stores the individual elements of the sql table like userid so it can then be used to display on a handlebar

---
+++ modules/questions.js
@@ -0,0 +1,44 @@
+ /** @module Questions */
+
+import sqlite from 'sqlite-async'
+
+
+/**
+ * Accounts
+ * ES6 module that handles registering accounts and logging in.
+ */
+class Questions {
+	/**
+   * Create an account object
+   * @param {String} [dbName=":memory:"] - The name of the database file to use.
+   */
+	constructor(dbName = ':memory:') {
+    return (async() => {
+			this.db = await sqlite.open(dbName)
+			// we need this table to store the user accounts
+			const sql = 'CREATE TABLE IF NOT EXISTS questions\
+				(id INTEGER PRIMARY KEY AUTOINCREMENT,\
+         userid INTEGER,\
+         firstname TEXT NOT NULL,\
+         lastname TEXT NOT NULL,\
+         title TEXT,\
+         summary TEXT,\
+         status TEXT,\
+         FOREIGN KEY(userid) REFERENCES users(id)\
+        );'
+			await this.db.run(sql)
+			return this
+		})()
+	}
+
+
+  async all()
+    {
+      const sql = 'SELECT users.user, questions.*FROM questions, users\
+                     WHERE questions.userid = users.id;'
+      const questions = await this.db.all(sql)
+      return questions
+    }
+}
+
+export default Questions
