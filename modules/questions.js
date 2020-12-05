266fc32e5aa42819e484d81b1e172233b7eaf434
Daniel Rodrigues <rodrig75@coventry.ac.uk>
2020-12-05 23:40:34 +0000

    answers format add a visual format for the answers.handlebars added a textfield and submit button for the format of the handlebar, organised a table for the answers to be inputted into

--- modules/questions.js
+++ modules/questions.js
@@ -25,6 +25,7 @@ class Questions {
          lastname TEXT NOT NULL,\
          title TEXT,\
          summary TEXT,\
+         description TEXT,\
          status TEXT,\
          FOREIGN KEY(userid) REFERENCES users(id)\
         );'
@@ -32,12 +33,11 @@ class Questions {
 			return this
 		})()
 	}
-
-
+  
   async all()
     {
-      const sql = 'SELECT users.user, questions.*FROM questions, users\
-                     WHERE questions.userid = users.id;'
+      const sql = `SELECT users.user, questions.*FROM questions, users\
+                     WHERE questions.userid = users.id;`
       const questions = await this.db.all(sql)
       for(const index in questions)
       {
@@ -56,9 +56,9 @@ class Questions {
       console.log(sql)
       const question = await this.db.get(sql) //gets sql query
       if(question.photo === null) question.photo = 'placeholder.jpeg' // if no picture default to this
-      const dateTime = new Date(question.lastcontact * 1000) //converts to from milliseconds
-      const date = `${dateTime.getDate()}/${dateTime.getMonth()+1}/${dateTime.getFullYear()}`
-      question.lastcontact = date // stores as date
+      //const dateTime = new Date(question.lastcontact * 1000) //converts to from milliseconds
+      //const date = `${dateTime.getDate()}/${dateTime.getMonth()+1}/${dateTime.getFullYear()}`
+      //question.lastcontact = date // stores as date
       return question // all fields within a single record
     } catch(err){
       console.log(err)
@@ -79,6 +79,33 @@ class Questions {
         await fs.copy(data.filePath, `public/avatars/${filename}`)
       }
       const sql = `INSERT INTO questions(userid, photo, firstname, lastname, title, summary, description)\
+                    Values(${data.account}, "${filename}", "${data.firstname}", "${data.lastname}",\
+                            "${data.title}", "${data.summary}", "${data.description}")`
+      console.log(sql)
+      await this.db.run(sql)
+      return true
+    }
+    catch(err)
+    {
+      console.log(err)
+      throw(err)
+    }
+    
+  }
+  
+  async postans(data)
+  {
+    console.log('POSTANS')
+    console.log(data)
+    let filename
+    try{
+      if(data.fileName)
+      {
+        filename = `${Date.now()}.${mime.extension(data.fileType)}` //current time 
+        console.log(filename)
+        await fs.copy(data.filePath, `public/avatars/${filename}`)
+      }
+      const sql = `INSERT INTO answers(userid, photo, firstname, lastname, title, summary, description)\
                     Values(${data.account}, "${filename}", "${data.firstname}", "${data.lastname}",\
                             "${data.title}", "${data.summary}", "${data.description}")`
       console.log(sql)
